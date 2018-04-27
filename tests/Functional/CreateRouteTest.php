13ba6567cc2eb26f0e0081ef79eee65aca86e99d
Ali Martin <alistairmartin@live.com>
2018-04-27 14:38:18 +0100

    added route tests

---
+++ tests/Functional/CreateRouteTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: c2904207
+ * Date: 27/04/2018
+ * Time: 14:02
+ */
+
+namespace Tests\Functional;
+
+
+class CreateRouteTest extends BaseTestCase
+{
+    /**
+     * Test that the /routes/new route returns a rendered response containing some expected text.
+     */
+    public function testGetRoutes()
+    {
+        $response = $this->runApp('GET', '/routes/new' . $this->ROUTE_ID);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertContains('You are creating a new route', (string)$response->getBody());
+    }
+}
